95462bcbf977c7a4db3ed6f4ccdbd3323e22e568
ivyroot <jacob@draup.xyz>
2023-10-23 22:00:08 -0700

    refactor Columns to component

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,19 @@ import { Stats, OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 import { TextDisplay } from './components/TextDisplay';
 import { Floor } from './components/Floor';
+import { CircularColumns } from './components/Columns';
 
 import './App.css';
-interface CirclePointsProps {
-  radius: number;
-  numPoints: number;
-}
 
 
 
-function generateCirclePoints(radius: number, numPoints: number): THREE.Vector3[] {
-  const points: THREE.Vector3[] = [];
-  const angleStep = 2 * Math.PI / numPoints; // Angle between each point in radians
-
-  for (let i = 0; i < numPoints; i++) {
-      const theta = i * angleStep;
-      const x = radius * Math.cos(theta);
-      const z = radius * Math.sin(theta);
-
-      points.push(new THREE.Vector3(x, 0, z));
-  }
-
-  return points;
-}
-
-const CirclePoints: React.FC<CirclePointsProps> = ({ radius, numPoints }) => {
-  const points = generateCirclePoints(radius, numPoints);
-  return (
-    <group>
-      {points.map((point, i) => (
-        <mesh key={i} position={point}>
-          <cylinderGeometry args={[0.92, 0.92, 80]} />
-          <meshStandardMaterial metalness={0.9} roughness={0.1}  color="#333399" />
-        </mesh>
-      ))}
-    </group>
-  );
-}
 
 const Scene = () => {
   return (
     <>
       <pointLight intensity={1000.0} position={[-5, -8, -5]} />
       <pointLight intensity={1000.0} position={[5, 8, 5]} />
-      <CirclePoints radius={10} numPoints={9} />
+      <CircularColumns radius={10} numColumns={9} thickness={1.0} height={80} />
       <TextDisplay
         position={[0,0,0]}
         url={'https://www.exquisitecanvas.xyz'}
---
+++ src/components/Columns.tsx
@@ -0,0 +1,37 @@
+import * as THREE from "three";
+
+export interface CircularColumnsProps {
+    radius: number;
+    numColumns: number;
+    thickness: number;
+    height?: number;
+}
+
+function generateCirclePoints(radius: number, numPoints: number): THREE.Vector3[] {
+    const points: THREE.Vector3[] = [];
+    const angleStep = 2 * Math.PI / numPoints; // Angle between each point in radians
+
+    for (let i = 0; i < numPoints; i++) {
+        const theta = i * angleStep;
+        const x = radius * Math.cos(theta);
+        const z = radius * Math.sin(theta);
+
+        points.push(new THREE.Vector3(x, 0, z));
+    }
+
+    return points;
+}
+
+export const CircularColumns: React.FC<CircularColumnsProps> = ({ radius, numColumns, thickness, height = 80 }) => {
+    const points = generateCirclePoints(radius, numColumns);
+    return (
+      <group>
+        {points.map((point, i) => (
+          <mesh key={i} position={point}>
+            <cylinderGeometry args={[thickness, thickness, height]} />
+            <meshStandardMaterial metalness={0.9} roughness={0.1}  color="#333399" />
+          </mesh>
+        ))}
+      </group>
+    );
+  }
